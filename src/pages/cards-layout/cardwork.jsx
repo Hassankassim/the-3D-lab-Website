a1b6ed53dfa0443579d74e9c6d2a432d05d083a1
HASSAN KASSIM ELIAS <114056773+Hassankassim@users.noreply.github.com>
2024-04-21 01:36:01 +0300

    4

--- src/pages/cards-layout/cardwork.jsx
+++ src/pages/cards-layout/cardwork.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 const CardWork = (props) => {
   return (
     <div className="col-sm center margint">
+      
       <div className="card-st">
+   
       <span className="">
+      <p className='headl2' >{props.details}</p>
+      <p className='headl2' >{props.details}</p>
               <img width="" className="center" id='roboto' src={props.imgSrc} alt="" />
               <p className='headl2' >{props.details}</p>
             </span>
